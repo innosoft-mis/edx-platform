c7c35d8a334e607abad058bac5b49324774b538e
singhanat <singha.osot@gmail.com>
2020-11-27 11:57:08 +0700

    ลองอีก

--- lms/static/js/discovery/models/course_discovery.js
+++ lms/static/js/discovery/models/course_discovery.js
@@ -7,55 +7,55 @@
     ], function(_, Backbone, CourseCard, FacetOption) {
         'use strict';
 
-        // return Backbone.Model.extend({
-        //     url: '/search/course_discovery/',
-        //     jqhxr: null,
+        return Backbone.Model.extend({
+            url: '/search/course_discovery/',
+            jqhxr: null,
 
-        //     defaults: {
-        //         totalCount: 0,
-        //         latestCount: 0
-        //     },
+            defaults: {
+                totalCount: 0,
+                latestCount: 0
+            },
 
-        //     initialize: function() {
-        //         this.courseCards = new Backbone.Collection([], {model: CourseCard});
-        //         this.facetOptions = new Backbone.Collection([], {model: FacetOption});
-        //     },
+            initialize: function() {
+                this.courseCards = new Backbone.Collection([], {model: CourseCard});
+                this.facetOptions = new Backbone.Collection([], {model: FacetOption});
+            },
 
-        //     parse: function(response) {
-        //         var courses = response.results || [];
-        //         var facets = response.facets || {};
-        //         this.courseCards.add(_.pluck(courses, 'data'));
+            parse: function(response) {
+                var courses = response.results || [];
+                var facets = response.facets || {};
+                this.courseCards.add(_.pluck(courses, 'data'));
 
-        //         this.set({
-        //             totalCount: response.total,
-        //             latestCount: courses.length
-        //         });
+                this.set({
+                    totalCount: response.total,
+                    latestCount: courses.length
+                });
 
-        //         var options = this.facetOptions;
-        //         _(facets).each(function(obj, key) {
-        //             _(obj.terms).each(function(count, term) {
-        //                 options.add({
-        //                     facet: key,
-        //                     term: term,
-        //                     count: count
-        //                 }, {merge: true});
-        //             });
-        //         });
-        //     },
+                var options = this.facetOptions;
+                _(facets).each(function(obj, key) {
+                    _(obj.terms).each(function(count, term) {
+                        options.add({
+                            facet: key,
+                            term: term,
+                            count: count
+                        }, {merge: true});
+                    });
+                });
+            },
 
-        //     reset: function() {
-        //         this.set({
-        //             totalCount: 0,
-        //             latestCount: 0
-        //         });
-        //         this.courseCards.reset();
-        //         this.facetOptions.reset();
-        //     },
+            reset: function() {
+                this.set({
+                    totalCount: 0,
+                    latestCount: 0
+                });
+                this.courseCards.reset();
+                this.facetOptions.reset();
+            },
 
-        //     latest: function() {
-        //         return this.courseCards.last(this.get('latestCount'));
-        //     }
+            latest: function() {
+                return this.courseCards.last(this.get('latestCount'));
+            }
 
-        // });
+        });
     });
 }(define || RequireJS.define));
