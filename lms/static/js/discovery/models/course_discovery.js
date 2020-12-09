8bfdd794f3197d792a7b3153547a9c3a6707b6f2
singhanat <singha.osot@gmail.com>
2020-12-09 12:31:30 +0700

    กลับไปเท่ากับ innosoft/juniper.2.1.0011

--- lms/static/js/discovery/models/course_discovery.js
+++ lms/static/js/discovery/models/course_discovery.js
@@ -23,6 +23,8 @@
 
             parse: function(response) {
                 var courses = response.results || [];
+                console.log(courses);
+                courses.sort((a, b) => (a._id > b._id) ? 1 : -1);
                 var facets = response.facets || {};
                 this.courseCards.add(_.pluck(courses, 'data'));
 
